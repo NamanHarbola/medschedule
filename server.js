9e32ef56aa6f80c2cfc3f9b4d2c956ec12c28420
NamanHarbola <namanharbola3@gmail.com>
2024-12-08 15:31:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = 4000;
 
-// Middleware to serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
@@ -39,7 +37,6 @@ app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
