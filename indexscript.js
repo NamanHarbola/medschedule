932cd20558c284e4bc78a9876f9218aca26195d4
NamanHarbola <namanharbola3@gmail.com>
2024-12-08 12:04:54 +0530

    Create indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,8 @@
+// Redirection Links
+document.getElementById('loginBtn').addEventListener('click', () => {
+    window.location.href = 'login.html'; // Redirect to login page
+});
+
+document.getElementById('signupBtn').addEventListener('click', () => {
+    window.location.href = 'signup.html'; // Redirect to signup page
+});
