5ed3fa89d7adfa049bccda8ec6b7fb8f3eac9ef5
NamanHarbola <namanharbola3@gmail.com>
2024-12-08 12:18:52 +0530

    Create logoutscript.js

---
+++ logoutscript.js
@@ -0,0 +1,6 @@
+// Add a little interaction to the button
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', () => {
+    alert('Redirecting to the welcome tab!');
+});
